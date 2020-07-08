fca610e9f7c1196b089f759023c87548c13d7829
Viktor Bezruk <44390837+bezrukviktor@users.noreply.github.com>
2020-07-08 14:16:00 +0300

    moved map to main js

--- js/main.js
+++ js/main.js
@@ -46,4 +46,15 @@ $(document).ready(function () {
   $('.input-tel').mask('+00 (000) 000 00 00', {
     placeholder: '+__ (___) ___ __ __'
   });
+
+  let start = $('.prices');
+  let startTop = start.offset().top;
+  $(window).bind('scroll', function () {
+    let windowTop = $(this).scrollTop();
+    console.log(windowTop)
+    if (windowTop > startTop) {
+      $('.map').html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1062.2086424670708!2d37.644139751011096!3d55.65870098930891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab36bf58f6ed5%3A0x2da28a2339bd09fa!2z0JrQsNGI0LjRgNGB0LrQvtC1INGILiwgMjMsIDM0NSwg0JzQvtGB0LrQstCwLCDQoNC-0YHRgdC40Y8sIDExNTQ3OA!5e0!3m2!1sru!2sfr!4v1593867958929!5m2!1sru!2sfr" width="100%" height="410" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>');
+      $(window).unbind('scroll')
+    }
+  })
 })
